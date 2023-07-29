f862dc408f0c783ff1f5405f328fb2904a2f1902
divyanka99 <divyach564564@gmail.com>
2023-07-29 23:53:55 +0530

    error

--- divya.py
+++ divya.py
@@ -18,4 +18,5 @@ for elems in questions:
             print('sorry you gave wrong answer youa r etaking only 3000 rupees')
         else:
             print('sorry you lost and won 0 rupees')
+            dsafas
         break
